73160d54578e050090eff0354eb30ad187c501c0
Moaad Amer <moaad02@gmail.com>
2025-02-03 12:38:23 +0200

    Tickets Data Aggregator app

---
+++ 10_Strings/Helpers/PathHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace _10_Strings.Helpers;
+
+public class PathHelper
+{
+    public static string GetBinFolderPath()
+    {
+        return Path.GetDirectoryName(
+              System.Reflection.Assembly.GetExecutingAssembly().Location);
+    }
+
+}
